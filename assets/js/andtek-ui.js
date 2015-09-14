f198afeb9af420ede743608d174371a8ec7d2b61
Jonas Friedmann <j@frd.mn>
2015-09-14 21:15:24 +0200

    Render agents in dummy list view

--- assets/js/andtek-ui.js
+++ assets/js/andtek-ui.js
@@ -9,3 +9,24 @@ var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
+
+// Wait until jQuery is loaded...
+$(document).ready(function(){
+  // Parse agents from JSON
+  $.getJSON('agents.json', function(agents) {
+    // Get rid of "Loading ..." dummy entry
+    $('ul').html(''); 
+    // For each agent ...
+    for (agent in agents) {
+      $('ul').append(
+        '<li class="item-content">' +
+        '  <div class="item-media"><i class="fa fa-circle ' + (agents[agent]['internal'] ? 'green' : 'red') + '"></i></div>' +
+        '  <div class="item-inner">' +
+        '    <div class="item-title">' + agent + '</div>' +
+        '    <div class="item-after"><i class="fa fa-circle ' + (agents[agent]['external'] ? 'green' : 'red') + '"></i></div>' +
+        '  </div>' +
+        '</li>'
+      );
+    }
+  });
+});
