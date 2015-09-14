46a1969ed1f105014d614189fe780ddc2722ecfc
Jonas Friedmann <j@frd.mn>
2015-09-14 23:49:46 +0200

    Cleanup

--- assets/js/andtek-ui.js
+++ assets/js/andtek-ui.js
@@ -15,15 +15,15 @@ $(document).ready(function(){
   // Parse agents from JSON
   $.getJSON('agents.json', function(agents) {
     // Get rid of "Loading ..." dummy entry
-    $('ul').html(''); 
+    $('ul').html('');
     // For each agent ...
-    for (agent in agents) {
+    for (var agent in agents) {
       $('ul').append(
         '<li class="item-content">' +
-        '  <div class="item-media"><i class="fa fa-circle ' + (agents[agent]['internal'] ? 'green' : 'red') + '"></i></div>' +
+        '  <div class="item-media"><i class="fa fa-circle ' + (agents[agent].internal ? 'green' : 'red') + '"></i></div>' +
         '  <div class="item-inner">' +
         '    <div class="item-title">' + agent + '</div>' +
-        '    <div class="item-after"><i class="fa fa-circle ' + (agents[agent]['external'] ? 'green' : 'red') + '"></i></div>' +
+        '    <div class="item-after"><i class="fa fa-circle ' + (agents[agent].external ? 'green' : 'red') + '"></i></div>' +
         '  </div>' +
         '</li>'
       );
