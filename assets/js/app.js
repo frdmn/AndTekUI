25043a7dccc3c2041e0c4d36cc8971622002f243
Jonas Friedmann <j@frd.mn>
2015-09-28 13:55:55 +0200

    Poor mans sprintf() replacement function

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,6 +57,37 @@ function loadConfigFile(callback){
 }
 
 /**
+ * Minimal sprintf() function
+ * by http://stackoverflow.com/a/4795914
+ * @return {String} Formatted string
+ */
+function sprintf() {
+    var args = arguments,
+    string = args[0],
+    i = 1;
+    return string.replace(/%((%)|s|d)/g, function (m) {
+        // m is the matched format, e.g. %s, %d
+        var val = null;
+        if (m[2]) {
+            val = m[2];
+        } else {
+            val = args[i];
+            // A switch statement so that the formatter can be extended. Default is %s
+            switch (m) {
+                case '%d':
+                    val = parseFloat(val);
+                    if (isNaN(val)) {
+                        val = 0;
+                    }
+                    break;
+            }
+            i++;
+        }
+        return val;
+    });
+}
+
+/**
  * Returns the status of an agent in a specific queue
  * @param  {String}   agentMacAddress
  * @param  {Integer}  queueId
